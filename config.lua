d7f0f3b83f64c45e04981dd3128762b92a66ef94
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-08-01 08:18:40 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,150 @@
+Config = {}
+Config.Locale = GetConvar('esx:locale', 'en')
+Config.Visible = true
+
+Config.Items = {
+	["cake"] = {
+		type = "food",
+		prop= "v_res_cakedome",
+		status = 250000,
+		remove = true
+	},
+	["joghurt"] = {
+		type = "fruit",
+		prop = "prop_cs_milk_01",
+		status = 50000,
+		remove = true
+	},
+	["redbull"] = {
+		type = "drink",
+		prop = "prop_ecola_can",
+		status = 200000,
+		remove = true
+	},
+	["chips"] = {
+		type = "food",
+		prop = "ng_proc_food_chips01a",
+		status = 150000,
+		remove = true
+	},
+	["coffee"] = {
+		type = "drink",
+		prop = "p_ing_coffeecup_02",
+		status = 200000,
+		remove = true
+	},
+	["tuna"] = {
+		type = "food",
+		prop = "v_ret_247_tuna",
+		status = 150000,
+		remove = true
+	},
+	["sandwich"] = {
+		type = "food",
+		prop = "prop_sandwich_01",
+		status = 100000,
+		remove = true
+	},
+	["soda"] = {
+		type = "drink",
+		prop = "ng_proc_sodacan_01a",
+		status = 200000,
+		remove = true
+	},
+	["pineapple"] = {
+		type = "fruit",
+		prop = "prop_pineapple",
+		status = 50000,
+		remove = true
+	},
+	["beer"] = {
+		type = "alcohol",
+		prop = "prop_beer_bottle",
+		status = 70000,
+		remove = true
+	},
+	["tequila"] = {
+		type = "alcohol",
+		prop = "prop_tequila_bottle",
+		status = 120000,
+		remove = true
+	},
+	["white_wine"] = {
+		type = "alcohol",
+		prop = "prop_wine_white",
+		status = 80000,
+		remove = true
+	},
+	["red_wine"] = {
+		type = "alcohol",
+		prop = "prop_wine_red",
+		status = 80000,
+		remove = true
+	},
+	["vodka"] = {
+		type = "alcohol",
+		prop = "prop_vodka_bottle",
+		status = 150000,
+		remove = true
+	},
+	["rhum"] = {
+		type = "alcohol",
+		prop = "prop_rum_bottle",
+		status = 180000,
+		remove = true
+	},
+	["protein_bar"] = {
+		type = "food",
+		prop = "prop_choc_ego",
+		status = 70000,
+		remove = true
+	},
+	["chocolate"] = {
+		type = "food",
+		prop = "prop_choc_pq",
+		status = 140000,
+		remove = true
+	},
+	["twix"] = {
+		type = "food",
+		prop = "prop_choc_meto",
+		status = 70000,
+		remove = true
+	},
+	["donut"] = {
+		type = "food",
+		prop = "prop_donut_01",
+		status = 80000,
+		remove = true
+	},
+	["donut2"] = {
+		type = "food",
+		prop = "prop_donut_02b",
+		status = 80000,
+		remove = true
+	},
+	["donut3"] = {
+		type = "food",
+		prop = "prop_amb_donut",
+		status = 80000,
+		remove = true
+	},
+	["apple"] = {
+		type = "food",
+		prop = "ng_proc_food_aple1a",
+		status = 50000,
+		remove = true
+	},
+	["banana"] = {
+		type = "food",
+		prop = "ng_proc_food_nana2a",
+		status = 50000,
+		remove = true
+	},
+	["orange"] = {
+		type = "food",
+		prop = "ng_proc_food_ornge1a",
+		status = 50000,
+		remove = true
+	}
+}
---
+++ esx_food&drinks.sql
@@ -0,0 +1,28 @@
+
+
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('apple', 'Apple', 1),
+	('banana', 'Banana', 1),
+	('beer', 'Beer', 1),
+	('cake', 'Cake', 1),
+	('chips', 'Chips', 1),
+	('chocolate', 'Chocolate', 1),
+	('coffee', 'Coffee', 1),
+	('donut', 'Donut', 1),
+	('donut2', 'Donut with crumbs', 1),
+	('donut3', 'vanilla donut', 1),
+	('yogurt', 'Yogurt', 1),
+	('orange', 'Orange', 1),
+	('protein_bar', 'Protein bar', 1),
+	('red_wine', 'Red wine', 1),
+	('redbull', 'Redbull', 1),
+	('rhum', 'Rhum', 1),
+	('sandwich', 'Sandwich', 1),
+	('soda', 'Soda', 1),
+	('tequila', 'Tequila', 1),
+	('tuna', 'Tuna', 1),
+	('twix', 'Twix', 1),
+	('vodka', 'Vodka', 1),
+	('white_wine', 'White wine', 1),
+	('pineapple', 'Pineapple', 1)
+	;
