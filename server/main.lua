093eacdc88c18d49b5813a21225ada2f47c3df40
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-08-01 08:20:56 +0200

    Add files via upload

--- server/main.lua
+++ server/main.lua
@@ -1,1 +1,28 @@
+CreateThread(function()
+	for k,v in pairs(Config.Items) do
+		ESX.RegisterUsableItem(k, function(source)
+			local xPlayer = ESX.GetPlayerFromId(source)
+			if v.remove then
+				xPlayer.removeInventoryItem(k,1)
+			end
+			if v.type == "food" then
+				TriggerClientEvent("esx_status:add", source, "hunger", v.status)
+				TriggerClientEvent('cynio:onUse', source, v.type, v.prop)
+			elseif v.type == "drink" then
+				TriggerClientEvent("esx_status:add", source, "thirst", v.status)
+				TriggerClientEvent('cynio:onUse', source, v.type, v.prop)
+			elseif v.type == "alcohol" then
+				TriggerClientEvent("esx_status:remove", source, "thirst", v.status)
+				TriggerClientEvent('cynio:onUse', source, v.type, v.prop)	
+			elseif v.type == "fruit" then
+				TriggerClientEvent("esx_status:add", source, "thirst", v.status)
+				TriggerClientEvent("esx_status:add", source, "hunger", v.status)
+				TriggerClientEvent('cynio:onUse', source, v.type, v.prop)	
+			else
+				print(string.format('^1[ERROR]^0 %s has no correct type defined.', k))
+			end
+		end)
+	end 
+end)
+
 
