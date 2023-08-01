f8028738910034ce7c012d40259d94263d626500
cynio <115213364+dominiksmolinski3@users.noreply.github.com>
2023-08-01 08:19:58 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,15 +10,11 @@ legacyversion '1.9.1'
 shared_script '@es_extended/imports.lua'
 
 server_scripts {
-    '@es_extended/locale.lua',
-    'locales/*.lua',
     'config.lua',
     'server/main.lua'
 }
 
 client_scripts {
-    '@es_extended/locale.lua',
-    'locales/*.lua',
     'config.lua',
     'client/main.lua'
 }
